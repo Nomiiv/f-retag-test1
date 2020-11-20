9d8ac12fcb3a441776b89f30fb0eb682bcbd9c65
Nomiiv <papernomiiv@gmail.com>
2020-11-20 02:01:25 +0100

    responsiv + delsidor + footer + fonts + slider fix ++

--- js/jason.js
+++ js/jason.js
@@ -1,22 +1,32 @@
 $(function () {
   $(".multiple-items").slick({
     centerMode: true,
-    slidesToShow: 3,
+    slidesToShow: 5,
     slidesToScroll: 1,
     arrows: true,
     autoplay: true,
     autoplaySpeed: 2000,
-    variableWidth: true,
     prevArrow:
       "<img class='a-left control-c prev slick-prev' src='pic/left.png'>",
     nextArrow:
       "<img class='a-right control-c next slick-next' src='pic/right.png'>",
     responsive: [
       {
+        breakpoint: 1440,
+        settings: {
+          arrows: true,
+          slidesToShow: 5,
+          centerMode: true,
+          variableWidth: true
+        },
+      },
+      {
         breakpoint: 768,
         settings: {
           arrows: false,
           slidesToShow: 1,
+          centerMode: true,
+          variableWidth: true
         },
       },
       {
@@ -24,6 +34,8 @@ $(function () {
         settings: {
           arrows: false,
           slidesToShow: 1,
+          centerMode: true,
+          variableWidth: true
         },
       },
     ],
